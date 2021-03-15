5f34e7cedd86d5554d34268a1cf8277174075106
- <tydyiu@qq.com>
2021-03-15 21:49:18 +0800

    一些备注

--- nanophase-security/src/main/java/com/nanophase/security/controller/SecurityController.java
+++ nanophase-security/src/main/java/com/nanophase/security/controller/SecurityController.java
@@ -1,11 +1,15 @@
 package com.nanophase.security.controller;
 
 import com.nanophase.common.util.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/hello")
 public class SecurityController {
@@ -37,12 +42,20 @@ public class SecurityController {
 
     /**
      * 该方法必须具备user角色才可以访问，但是必须加上ROLE_前缀 并且开启注解支持 securedEnabled = true
+     *  PostAuthorize("returnObject = 1") Spring EL 表达式 根据返回值判断是否有权限
+     *  返回结果可以是对象，可以是其他类型，表达式为true时，正常返回； 反之 返回403
      *
      * @return
      */
     @GetMapping("/get")
-    @Secured({"ROLE_user"})
+//    @Secured({"ROLE_user"})
+    @PostAuthorize("returnObject = 1")
     public String hello1() {
+        // Security封装 可以在任意的位置获取user信息
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object userName = authentication.getPrincipal();
+        log.info(userName.toString());
+        log.info(authentication.getAuthorities().toString());
         return "hello user";
     }
 
